88d36548e82402046b0e1d879557e46c0e7e68e5
amburkee <allie_burkeen@msn.com>
2021-01-26 13:18:04 -0500

    first upload

---
+++ wc/src/main/scala/example/WcDriver.scala
@@ -0,0 +1,58 @@
+package example
+
+import org.apache.hadoop.mapreduce.Job
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.io.Text
+import org.apache.hadoop.io.IntWritable
+
+
+/**
+  * WcDriver is the entrypoint for out word counting mapreduce
+  * 
+  * in this class we configure input and output data types, 
+  * we configure Mapper and Reducer classes, and specify intermediate data formats
+  * We're going to make use of command line arguments passed to this program
+  * we can access them with "args"
+  */
+object WcDriver {
+  def main(args : Array[String]): Unit = {
+    if(args.length != 2) {
+      println("Usage: wc <input dir> <output dir>")
+      System.exit(-1)
+    }
+
+    //we start by instantiating a Job object we can configure
+    val job = Job.getInstance()
+
+    //we set the jar file that countains our job -- we specify this class
+    job.setJarByClass(WcDriver.getClass())
+
+    job.setJobName("Intro Word Count (wc)")
+
+    //we set the input format, we're (almost) always going to be dealing with TextInputFormat
+    job.setInputFormatClass(classOf[TextInputFormat])
+
+    //set the file input and output based on the args passed in
+    FileInputFormat.setInputPaths(job, new Path(args(0)))
+    FileOutputFormat.setOutputPath(job, new Path(args(1)))
+
+    //The above is mostly boilerplate, you'll want something very similar in all your MR jobs
+    // below here we configure specific mappers and reducers, and we set the output key-value pair
+    // types based on the specifics -- what our job does.
+
+    //Specify mapper and reducer:
+    job.setMapperClass(classOf[WordMapper])
+    job.setReducerClass(classOf[WordReducer])
+
+    //specify output types, we're making use of some defaults to not specify more
+    job.setOutputKeyClass(classOf[Text])
+    job.setOutputValueClass(classOf[IntWritable])
+
+    val success = job.waitForCompletion(true) //submit configured job + wait for it to be done
+    System.exit(if (success) 0 else 1) //exit with 0 if successful
+
+  }
+}
